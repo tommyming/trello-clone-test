970701877102ccf6cdf3d97a6b5823126cf24759
Tommy Han <tommyhonym@gmail.com>
2022-12-15 14:56:45 +0800

    Add BoardList View with Display Logic.

---
+++ TrelloClone/Views/BoardListView.swift
@@ -0,0 +1,78 @@
+//
+//  BoardListView.swift
+//  TrelloClone
+//
+//  Created by tommyhan on 15/12/2022.
+//
+
+import SwiftUI
+
+struct BoardListView: View {
+    
+    @ObservedObject var board: Board
+    @StateObject var boardList: BoardList
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            headerView
+            listView
+                .listStyle(.plain)
+            
+            Button("+ Add card") {
+                
+            }
+            .padding(.horizontal)
+            .frame(maxWidth: .infinity, alignment: .center)
+        }
+        .padding(.vertical)
+        .background(boardListBackgroundColor)
+        .frame(width: 300)
+        .cornerRadius(8)
+        .foregroundColor(.black)
+    }
+    
+    private var headerView: some View {
+        HStack(alignment: .top) {
+            Text(boardList.name)
+                .font(.headline)
+                .lineLimit(2)
+            
+            Spacer()
+            
+            Menu {
+                Button("Rename") {
+                }
+                
+                Button("Delete", role: .destructive) {
+                }
+            } label: {
+                Image(systemName: "ellipsis.circle")
+                    .imageScale(.large)
+            }
+
+        }
+        .padding(.horizontal)
+    }
+    
+    private var listView: some View {
+        List {
+            ForEach(boardList.cards) { card in
+                Text(card.content)
+            }
+            .listRowSeparator(.hidden)
+            .listRowInsets(.init(top: 4, leading: 8, bottom: 4, trailing: 8))
+            .listRowBackground(Color.clear)
+        }
+    }
+}
+
+struct BoardListView_Previews: PreviewProvider {
+    
+    @StateObject static var board = Board.stub
+    
+    static var previews: some View {
+        BoardListView(board: board, boardList: board.lists[0])
+            .previewLayout(.sizeThatFits)
+            .frame(width: 300, height: 512)
+    }
+}
